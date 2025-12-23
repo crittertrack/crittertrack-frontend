de7d7c48be6074dab77e62ef784a53ecc2731be4
crittertrackowner <crittertrackowner@gmail.com>
2025-12-23 12:34:39 +0100

    Fix: Standardize c-dilute gene notation to c/variant format

--- src/components/MouseGeneticsCalculator.jsx
+++ src/components/MouseGeneticsCalculator.jsx
@@ -28,9 +28,9 @@ const GENE_LOCI = {
 
     combinations: [
       'c/c',
-      'ch/c', 'ch/ch',
-      'ce/c', 'ce/ch', 'ce/ce',
-      'cch/c', 'cch/ch', 'cch/ce', 'cch/cch',
+      'c/ch', 'ch/ch',
+      'c/ce', 'ce/ch', 'ce/ce',
+      'c/cch', 'ch/cch', 'ce/cch', 'cch/cch',
       'C/c', 'C/ch', 'C/ce', 'C/cch', 'C/C'
     ]
   },
