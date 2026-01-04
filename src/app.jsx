afcb3d4f973cdbcfb9f62a84ca30bcaefba9d44f
crittertrackowner <crittertrackowner@gmail.com>
2026-01-04 20:34:03 +0100

    Fix image modal scope - place in main App component return

--- src/app.jsx
+++ src/app.jsx
@@ -16304,6 +16304,41 @@ const App = () => {
                     </div>
                 </div>
             )}
+
+            {/* Image Modal */}
+            {showImageModal && enlargedImageUrl && (
+                <div 
+                    className="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50 p-4"
+                    onClick={() => setShowImageModal(false)}
+                >
+                    <div className="relative max-w-7xl max-h-full">
+                        <button
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setShowImageModal(false);
+                            }}
+                            className="absolute -top-12 right-0 text-white hover:text-gray-300 transition"
+                        >
+                            <X size={32} />
+                        </button>
+                        <img 
+                            src={enlargedImageUrl} 
+                            alt="Enlarged view" 
+                            className="max-w-full max-h-[90vh] object-contain"
+                            onClick={(e) => e.stopPropagation()}
+                        />
+                        <a
+                            href={enlargedImageUrl}
+                            download
+                            className="absolute -bottom-16 left-1/2 transform -translate-x-1/2 bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg flex items-center gap-2 transition"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <Download size={20} />
+                            Download Image
+                        </a>
+                    </div>
+                </div>
+            )}
             </div>
         </div>
     );
@@ -16404,41 +16439,6 @@ const PublicAnimalPage = () => {
                     currentPage={location.pathname}
                     currentContext={modCurrentContext}
                 />
-            )}
-
-            {/* Image Modal */}
-            {showImageModal && enlargedImageUrl && (
-                <div 
-                    className="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50 p-4"
-                    onClick={() => setShowImageModal(false)}
-                >
-                    <div className="relative max-w-7xl max-h-full">
-                        <button
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                setShowImageModal(false);
-                            }}
-                            className="absolute -top-12 right-0 text-white hover:text-gray-300 transition"
-                        >
-                            <X size={32} />
-                        </button>
-                        <img 
-                            src={enlargedImageUrl} 
-                            alt="Enlarged view" 
-                            className="max-w-full max-h-[90vh] object-contain"
-                            onClick={(e) => e.stopPropagation()}
-                        />
-                        <a
-                            href={enlargedImageUrl}
-                            download
-                            className="absolute -bottom-16 left-1/2 transform -translate-x-1/2 bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg flex items-center gap-2 transition"
-                            onClick={(e) => e.stopPropagation()}
-                        >
-                            <Download size={20} />
-                            Download Image
-                        </a>
-                    </div>
-                </div>
             )}
         </div>
     );
