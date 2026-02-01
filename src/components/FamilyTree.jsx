db43d48100cd84aa09b98dfe7efaf8c4db01d62c
crittertrackowner <crittertrackowner@gmail.com>
2026-02-01 13:37:19 +0100

    feat: set default zoom to 1x

--- src/components/FamilyTree.jsx
+++ src/components/FamilyTree.jsx
@@ -603,7 +603,7 @@ const FamilyTree = ({ authToken, userProfile, onViewAnimal, showModalMessage, on
                     onNodeClick={onNodeClick}
                     nodeTypes={nodeTypes}
                     fitView
-                    fitViewOptions={{ padding: 0.05, minZoom: 2, maxZoom: 2 }}
+                    fitViewOptions={{ padding: 0.05, minZoom: 1, maxZoom: 1 }}
                     attributionPosition="bottom-left"
                     className="bg-gray-50"
                     defaultEdgeOptions={{
