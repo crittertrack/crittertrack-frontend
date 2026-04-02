d076dbc4111764ff88647d20340fac1aea21b030
Crittertrack <crittertrackowner@gmail.com>
2026-04-02 18:05:13 +0200

    Bump service worker cache to v14 to force cache invalidation

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for CritterTrack PWA
-const CACHE_NAME = 'crittertrack-v13'; // Increment version to force cache update
+const CACHE_NAME = 'crittertrack-v14'; // Increment version to force cache update
 const urlsToCache = [
   '/',
   '/index.html',
