423362f1d2bb153e3cee0f70f362b8c4ca041461
Crittertrack <crittertrackowner@gmail.com>
2026-06-09 19:21:49 +0200

    Bump service worker cache version to v29 to force cache clear

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for CritterTrack PWA
-const CACHE_NAME = 'crittertrack-v28'; // Increment version to force cache update
+const CACHE_NAME = 'crittertrack-v29'; // Increment version to force cache update
 const urlsToCache = [
   '/',
   '/index.html',
