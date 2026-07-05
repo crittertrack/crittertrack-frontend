06d5f70c5280e4762c99803c88984759c98cfd63
Crittertrack <crittertrackowner@gmail.com>
2026-07-05 16:48:53 +0200

    x

--- src/components/PublicProfile/PublicProfileView.jsx
+++ src/components/PublicProfile/PublicProfileView.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useRef, useMemo } from 'react';
 import axios from 'axios';
 import { useNavigate, useLocation, NavLink } from 'react-router-dom';
 import {
