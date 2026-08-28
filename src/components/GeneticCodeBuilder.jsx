3cc7f93dccd8f6f4d95506d0b501425f733b0548
Crittertrack <crittertrackowner@gmail.com>
2026-08-28 15:15:45 +0200

    Sync GeneticCodeBuilder UI dropdowns with new ball python loci (huf, cyp, gra, ob, lmb, dad)

--- src/components/GeneticCodeBuilder.jsx
+++ src/components/GeneticCodeBuilder.jsx
@@ -104,7 +104,7 @@ function buildRussianDwarfGeneticCode(genotype) {
     .join(' ');
 }
 
-const BALL_PYTHON_GENE_ORDER = ['cinBp', 'ban', 'cg', 'pas', 'en', 'fi', 'cho', 'van', 'pin', 'puz', 'wom', 'sab', 'od', 'yb', 'grv', 'spc', 'rus', 'spe', 'mys', 'chn', 'hon', 'bam', 'sch', 'spn', 'asp', 'jol', 'lor', 'raz', 'bon', 'jav', 'bld', 'cal', 'dst', 'dsc', 'bh', 'spk', 'jag', 'krg', 'ahi', 'dot', 'web', 'acd', 'bng', 'caf', 'gob', 'grm', 'jed', 'nov', 'bad', 'bgo', 'blz', 'blt', 'crm', 'gnx', 'nya', 'orb', 'pxl', 'qke', 'sat', 'hur', 'cpr', 'fsn', 'ksm', 'rpr', 'stc', 'trj', 'wke', 'zwd', 'mar', 'pch', 'rvn', 'sgr', 'vdo', 'mos', 'rgn', 'rhd', 'stk', 'tar', 'mck', 'stg', 'nny', 'cha', 'sp', 'les', 'moj', 'but', 'pha', 'pi', 'cl', 'gs', 'albCdy', 'lacGhi', 'ax', 'hy', 'cml', 'dg', 'sun', 'rax', 'tof', 'lav', 'ult', 'leo', 'cry', 'mig', 'vnt', 'zbr', 'shr', 'rbw', 'snt', 'spd', 'pnt'];
+const BALL_PYTHON_GENE_ORDER = ['cinBp', 'ban', 'cg', 'pas', 'en', 'fi', 'cho', 'van', 'pin', 'puz', 'wom', 'sab', 'od', 'yb', 'grv', 'spc', 'ob', 'rus', 'spe', 'mys', 'chn', 'hon', 'bam', 'sch', 'spn', 'asp', 'jol', 'lor', 'raz', 'bon', 'huf', 'cyp', 'gra', 'jav', 'bld', 'cal', 'dst', 'dsc', 'bh', 'spk', 'jag', 'krg', 'ahi', 'dot', 'web', 'acd', 'bng', 'caf', 'gob', 'grm', 'jed', 'nov', 'bad', 'bgo', 'blz', 'blt', 'crm', 'gnx', 'nya', 'orb', 'pxl', 'qke', 'sat', 'hur', 'cpr', 'fsn', 'ksm', 'rpr', 'stc', 'trj', 'wke', 'zwd', 'mar', 'pch', 'rvn', 'sgr', 'vdo', 'mos', 'rgn', 'rhd', 'stk', 'tar', 'mck', 'stg', 'nny', 'lmb', 'cha', 'sp', 'les', 'moj', 'but', 'pha', 'dad', 'pi', 'cl', 'gs', 'albCdy', 'lacGhi', 'ax', 'hy', 'cml', 'dg', 'sun', 'rax', 'tof', 'lav', 'ult', 'leo', 'cry', 'mig', 'vnt', 'zbr', 'shr', 'rbw', 'snt', 'spd', 'pnt'];
 
 function parseBallPythonGeneticCode(codeString) {
   if (!codeString) return {};
@@ -1164,9 +1164,9 @@ const GeneticCodeBuilder = ({ species, gender, value, onChange, onOpenCommunityF
   if (species === 'Ball Python') {
     const BALL_PYTHON_GENE_GROUPS = [
       { label: 'Cinnamon / Black Pastel (multi-allelic)', loci: ['cinBp'] },
-      { label: 'Incomplete Dominant Genes',                loci: ['pas', 'en', 'fi', 'cho', 'van', 'pin', 'puz', 'wom', 'sab', 'od', 'ban', 'cg', 'yb', 'grv', 'spc', 'rus', 'spe', 'mys', 'chn', 'hon', 'bam', 'sch', 'spn', 'asp', 'jol', 'lor', 'raz', 'bon', 'jav', 'bld', 'dsc', 'bh', 'spk', 'jag', 'dot', 'bng', 'gob', 'grm', 'jed', 'blt', 'crm', 'gnx', 'orb', 'pxl', 'qke', 'sat', 'hur', 'cpr', 'fsn', 'ksm', 'rpr', 'trj', 'wke', 'zwd', 'mar', 'pch', 'rvn', 'vdo', 'mos', 'rhd', 'stk', 'tar', 'mck', 'nny', 'cha'] },
+      { label: 'Incomplete Dominant Genes',                loci: ['pas', 'en', 'fi', 'cho', 'van', 'pin', 'puz', 'wom', 'sab', 'od', 'ban', 'cg', 'yb', 'grv', 'spc', 'ob', 'rus', 'spe', 'mys', 'chn', 'hon', 'bam', 'sch', 'spn', 'asp', 'jol', 'lor', 'raz', 'bon', 'huf', 'cyp', 'gra', 'jav', 'bld', 'dsc', 'bh', 'spk', 'jag', 'dot', 'bng', 'gob', 'grm', 'jed', 'blt', 'crm', 'gnx', 'orb', 'pxl', 'qke', 'sat', 'hur', 'cpr', 'fsn', 'ksm', 'rpr', 'trj', 'wke', 'zwd', 'mar', 'pch', 'rvn', 'vdo', 'mos', 'rhd', 'stk', 'tar', 'mck', 'nny', 'lmb', 'cha'] },
       { label: 'Dominant Genes',                            loci: ['sp', 'cal', 'dst', 'krg', 'ahi', 'web', 'acd', 'caf', 'nov', 'bad', 'bgo', 'blz', 'nya', 'stc', 'sgr', 'rgn', 'stg'] },
-      { label: 'BEL Complex (Lesser/Mojave/Butter/Phantom)', loci: ['les', 'moj', 'but', 'pha'] },
+      { label: 'BEL Complex (Lesser/Mojave/Butter/Phantom/Daddy)', loci: ['les', 'moj', 'but', 'pha', 'dad'] },
       { label: 'Albino / Candy (multi-allelic)',            loci: ['albCdy'] },
       { label: 'Lace / GHI (multi-allelic)',                 loci: ['lacGhi'] },
       { label: 'Recessive Genes',                            loci: ['pi', 'cl', 'gs', 'ax', 'hy', 'cml', 'dg', 'sun', 'rax', 'tof', 'lav', 'ult', 'leo', 'cry', 'mig', 'vnt', 'zbr', 'shr', 'rbw', 'snt', 'spd', 'pnt'] },
